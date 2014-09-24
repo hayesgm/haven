d9dc97e60baadc347fb1b9d61fee7951f8792d83
Geoff Hayes <hayesgm@gmail.com>
2014-09-23 23:50:45 -0700

    Add a simple home controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
 
   devise_for :tenants
-  root to: "home#index"
+  
+  root to: "home#home"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
